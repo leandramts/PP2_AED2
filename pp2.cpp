f5e1dc972cefc9ee2228a20a9aa09a8ea82872b0
Leandra Matos <leandra.mts10@gmail.com>
2025-09-06 22:24:57 -0400

    Fila criada, leitura da entrada e analise das distancias

--- pp2.cpp
+++ pp2.cpp
@@ -3,6 +3,7 @@
 #include <list>
 #include <vector>
 #include <stdexcept>
+#include <limits>
 
 typedef unsigned int uint;
 typedef unsigned int Vertex;
@@ -56,28 +57,102 @@ class GraphAl {
         {
             return num_edges;
         }
+};
 
-        void print_adjacency_list(GraphAl& g) const
+class Queue 
+{
+    private:
+    std::list<Vertex> list;
+
+    public:
+    Queue(){}
+
+    void enqueue(Vertex v)
+    {
+        list.push_back(v);
+    }
+
+    void dequeue()
+    {
+        list.pop_front();
+    }
+
+    Vertex front()
+    {
+        return list.front();
+    }
+
+    bool empty()
+    {
+        return list.empty();
+    }
+
+};
+
+enum class Color
+{
+    white, gray, black
+};
+
+class BFS
+{
+    private:
+        GraphAl& g;
+        std::vector <Color> color;
+        std::vector <uint> dist;
+        std::vector <int> pred;
+    
+    public:
+         BFS(GraphAl& graph)
+        : g(graph),
+          color(graph.get_num_vertices(), Color::white),
+          dist(graph.get_num_vertices(), std::numeric_limits<uint>::max()),
+          pred(graph.get_num_vertices(), -1) {}
+    
+    uint get_distance (Vertex v)
+    {
+        return dist[v];
+    }
+    void processBFS(Vertex s)
+    {
+        uint n = g.get_num_vertices();
+
+        for (Vertex u = 0; u < n; u++)
         {
-           uint n = g.get_num_vertices();
-           uint e = g.get_num_edges();
-           std:: cout << "num_vertices: " << n << std:: endl;
-           std:: cout << "num_edges: " << e << std:: endl;
-           for(uint u = 0; u < n; u++)
-           {
-                std:: list<Vertex> l = g.get_adj(u);
-                std:: cout << u << ": ";         
-                for (auto item: l)
-                {
-                    std:: cout << item << ", ";
-                  
-                }
+            color[u] = Color:: white; //pinta todos os vertices de branco
+            dist[u] = std::numeric_limits<uint>::max(); //considera primeiramente distancia infinita
+            pred[u] = -1; //predecessor "nulo"
+        }
+        
+        color[s]= Color::gray; //pinta a origem de cinza
+        dist[s] = 0;
+        pred[s] = -1;
 
-                std:: cout << std:: endl;
+        Queue Q;
 
-           }
+        Q.enqueue(s); //cria uma fila e bota o vertice de origem
+
+        while (!Q.empty()) //loop enquanto a fila nao estiver vazia
+        {
+            Vertex u = Q.front(); //analisa o vertice da frente na fila
+            Q.dequeue(); //tira o primeiro da fila
+            for (const auto& v : g.get_adj(u)) //loop para pegar os vertices adjacentes do vertice u que estava na frente da fila //!!!!acho que aqui que precisa botar o algoritmo de ordenacao
+            {
+            if (color[v] == Color::white) //se for branco (ainda nao encontrado)
+            {
+                color[v] = Color::gray; //pinta de cinza, aumenta o numero da distancia e adiciona o predecessor
+                dist[v] = dist[u] + 1; 
+                pred[v] = u;
+                Q.enqueue(v); //bote os vertices na fila
+            }
+
+            color[u] = Color::black; //depois de analisar todos os adjacentes do vertice, pinte de preto
+
+            }
 
         }
+
+    }
 };
 
 
@@ -121,23 +196,56 @@ void horses_paths(GraphAl& g)
 }
 
 
+Vertex position_to_vertice(std::string position)
+{
+    int initial_row = position[0] - 'a'; 
+    int initial_line = position[1] - '1'; 
+
+    return initial_line*8 + initial_row;
+}
+
+void process_tests(GraphAl& g, int tests) 
+{
+    for (int t = 0; t < tests; t++) 
+    {
+        std::string horse1, horse2, horse3, horse4, king;
+        std::cin >> horse1 >> horse2 >> horse3 >> horse4 >> king; // lê as entradas das posicoes iniciais dos cavalos e do rei, em string.
+
+        Vertex v_horse1 = position_to_vertice(horse1); //transforma a string em um numero de vertice do grafo
+        Vertex v_horse2 = position_to_vertice(horse2);
+        Vertex v_horse3 = position_to_vertice(horse3);
+        Vertex v_horse4 = position_to_vertice(horse4);
+        Vertex v_king   = position_to_vertice(king);
+
+        BFS bfs(g);  // executa a BFS a partir do rei
+        bfs.processBFS(v_king);
+
+        // pega distâncias de cada cavalo para ameacar o rei
+        int dist_h1 = bfs.get_distance(v_horse1) - 1 ;
+        int dist_h2 = bfs.get_distance(v_horse2) - 1;
+        int dist_h3 = bfs.get_distance(v_horse3) - 1;
+        int dist_h4 = bfs.get_distance(v_horse4) - 1;
+
+        // imprime resultado do caso de teste
+        std::cout << "Caso " << t+1 << ":\n";
+        std::cout << horse1 << " -> " << dist_h1 << "\n";
+        std::cout << horse2 << " -> " << dist_h2 << "\n";
+        std::cout << horse3 << " -> " << dist_h3 << "\n";
+        std::cout << horse4 << " -> " << dist_h4 << "\n";
+        std::cout << std::endl;
+    }
+}
+
+
 int main() {
     GraphAl g(64); // 64 vértices do tabuleiro
 
     horses_paths(g);
 
-    std::cout << "Numero de vertices: " << g.get_num_vertices() << std::endl;
-    std::cout << "Numero de arestas: " << g.get_num_edges() << std::endl;
-    std::cout << "---------------------------------------" << std::endl;
-
-    // percorre todas as casas (0 a 63)
-    for (Vertex u = 0; u < g.get_num_vertices(); u++) {
-        std::cout << u << ": ";
-        for (auto vizinho : g.get_adj(u)) {
-            std::cout << vizinho << " ";
-        }
-        std::cout << std::endl;
-    }
+    int tests;
+    std::cin >> tests;
+    
+    process_tests(g, tests);
 
     return 0;
 }
